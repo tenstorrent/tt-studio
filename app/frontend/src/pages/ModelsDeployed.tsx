30e553c0fdcad06932045fcac5c84f941e109104
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-06-10 16:43:24 -0400

    clean up

--- app/frontend/src/pages/ModelsDeployed.tsx
+++ app/frontend/src/pages/ModelsDeployed.tsx
@@ -2,8 +2,7 @@ import { ModelsDeployedTable } from "../components/ModelsDeployedTable";
 
 const ModelsDeployed = () => {
   return (
-    // <div className="flex flex-col h-screen w-full md:px-20 pt-8 overflow-hidden">
-    <div className=" h-screen flex-1 w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+    <div className=" h-screen flex-1 w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center ">
       {/* Radial gradient for the container to give a faded look */}
       <div
         className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white"
