f7dc364604d42a718abe36fde5fe295d413f26c5
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-06-24 14:36:01 -0400

    add more common themes

--- app/frontend/src/theme/commonThemeClasses.tsx
+++ app/frontend/src/theme/commonThemeClasses.tsx
@@ -8,6 +8,15 @@ interface CommonClasses {
   hoverBackgroundColor: string;
 }
 
+interface TableCommonClasses {
+  textColor: string;
+  backgroundColor: string;
+  borderColor: string;
+  tableCaptionColor: string;
+  tableRowBgColor: string;
+  tableRowFadeBgColor: string;
+}
+
 export default function useCommonClasses(): CommonClasses {
   const { theme } = useTheme();
 
@@ -25,6 +34,31 @@ export default function useCommonClasses(): CommonClasses {
       hoverTextColor,
       activeBorderColor,
       hoverBackgroundColor,
+    };
+  }, [theme]);
+}
+
+export function useTableCommonClasses(): TableCommonClasses {
+  const { theme } = useTheme();
+
+  return useMemo(() => {
+    const textColor = theme === "dark" ? "text-zinc-200" : "text-black";
+    const backgroundColor = theme === "dark" ? "bg-zinc-900" : "bg-white";
+    const borderColor =
+      theme === "dark" ? "border-zinc-400" : "border-gray-500";
+    const tableCaptionColor =
+      theme === "dark" ? "text-zinc-400" : "text-gray-500";
+    const tableRowBgColor = theme === "dark" ? "bg-zinc-900" : "bg-zinc-200";
+    const tableRowFadeBgColor =
+      theme === "dark" ? "bg-zinc-700" : "bg-zinc-200";
+
+    return {
+      textColor,
+      backgroundColor,
+      borderColor,
+      tableCaptionColor,
+      tableRowBgColor,
+      tableRowFadeBgColor,
     };
   }, [theme]);
 }
