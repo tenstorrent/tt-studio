7d2114b1867005197654db5321ca84120c8314ef
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-07-22 15:31:54 -0400

    Ui color fixes for main page footer

--- app/frontend/src/components/StepperFooter.tsx
+++ app/frontend/src/components/StepperFooter.tsx
@@ -23,17 +23,17 @@ const StepperFooter = ({
     <div className="flex items-center justify-end gap-2">
       <Button
         onClick={handleReset}
-        className="flex items-center gap-2 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white dark:bg-blue-700 dark:hover:bg-blue-800 dark:text-blue-100 rounded-lg"
+        className="flex items-center gap-2 px-4 py-2 bg-gray-700 hover:bg-gray-900 text-white rounded-lg dark:bg-gray-200 dark:hover:bg-gray-400 dark:text-black"
       >
         <RefreshCw className="w-5 h-5" />
         Reset and Deploy Another Model!
       </Button>
 
-      <Button className="flex items-center gap-2 px-4 py-2 border border-blue-500 text-white hover:bg-blue-300 hover:text-white dark:border-blue-400 dark:text-white dark:hover:bg-blue-400 dark:hover:text-white rounded-lg group">
+      <Button className="flex items-center gap-2 px-4 py-2 bg-gray-700 hover:bg-gray-900 text-white rounded-lg dark:bg-gray-200 dark:hover:bg-gray-400 dark:text-black">
         <Package className="w-5 h-5" />
         <Link
           to={"/models-deployed"}
-          className="no-underline text-white group-hover:text-white dark:text-white dark:group-hover:text-white"
+          className="no-underline text-white hover:text-white dark:text-black dark:hover:text-black"
         >
           Models Deployed
         </Link>
