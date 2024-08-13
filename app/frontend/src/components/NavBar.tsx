912c1ccb4f7f8775f5a127b57b70989e17743f67
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-08-13 14:37:50 -0600

    fix more conficts on navbar

--- app/frontend/src/components/NavBar.tsx
+++ app/frontend/src/components/NavBar.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useRef } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useRef } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import logo from "../assets/tt_logo.svg";
 import {
   NavigationMenu,
@@ -23,7 +23,7 @@ import Sidebar from "./SideBar";
 
 const NavBar: React.FC = () => {
   const { theme } = useTheme();
-  const [chatUIActive] = useState(false);
+  const location = useLocation();
   const sidebarRef = useRef<{ toggleSidebar: () => void }>(null);
 
   const iconColor = theme === "dark" ? "text-zinc-200" : "text-black";
@@ -36,7 +36,7 @@ const NavBar: React.FC = () => {
     theme === "dark" ? "hover:bg-zinc-700" : "hover:bg-gray-300";
 
   const navLinkClass = (isActive: boolean): string =>
-    `flex items-center px-2 py-2 rounded-md text-sm font-medium ${textColor} transition-all duration-300 ease-in-out ${
+    `flex items-center justify-center px-3 py-3 rounded-md text-sm font-medium ${textColor} transition-all duration-300 ease-in-out ${
       isActive ? `border-2 ${activeBorderColor}` : "border-transparent"
     } ${hoverTextColor} ${hoverBackgroundColor}`;
 
@@ -46,113 +46,148 @@ const NavBar: React.FC = () => {
     }
   };
 
+  const isChatUI = location.pathname === "/chat-ui";
+
   return (
     <TooltipProvider>
-      <div className="relative w-full">
+      <div
+        className={`${
+          isChatUI
+            ? "fixed top-0 left-0 h-full w-20 flex flex-col justify-between items-center pt-6 pb-6"
+            : "relative w-full flex justify-between items-center px-4 py-2 sm:px-5 sm:py-3"
+        } bg-secondary shadow-xl z-50`}
+      >
         <CustomToaster />
-        <div className="flex items-center justify-between w-full px-4 py-2 sm:px-5 sm:py-3 bg-secondary border-b-4 rounded-2xl shadow-xl">
-          <div className="flex items-center space-x-4 sm:space-x-6">
-            <a
-              href="https://www.tenstorrent.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center"
-            >
-              <img
-                src={logo}
-                alt="Tenstorrent Logo"
-                className="w-10 h-10 sm:w-14 sm:h-14 transform transition duration-300 hover:scale-110"
-              />
+        <div
+          className={`font-tt_a_mono flex ${
+            isChatUI
+              ? "flex-col items-center space-y-6"
+              : "flex items-center space-x-6"
+          }`}
+        >
+          <a
+            href="https://www.tenstorrent.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`flex items-center ${
+              isChatUI ? "mb-6 justify-center" : ""
+            }`}
+          >
+            <img
+              src={logo}
+              alt="Tenstorrent Logo"
+              className="w-10 h-10 sm:w-14 sm:h-14 transform transition duration-300 hover:scale-110"
+            />
+            {!isChatUI && (
               <h4
-                className={`hidden sm:block text-lg sm:text-2xl font-medium ${textColor} ml-3 bold font-roboto`}
+                className={`hidden sm:block text-lg sm:text-2xl font-tt_a_mono ${textColor} ml-3 bold font-roboto`}
               >
-                llm-studio
+                LLM Studio
               </h4>
-            </a>
-            <NavigationMenu className="flex-grow px-28">
-              <NavigationMenuList className="flex justify-between">
-                <NavigationMenuItem>
-                  <NavLink
-                    to="/"
-                    className={({ isActive }) => navLinkClass(isActive)}
-                  >
-                    <Home className={`mr-1 ${iconColor}`} />
-                    <span className="hidden sm:inline">Home</span>
-                  </NavLink>
-                </NavigationMenuItem>
+            )}
+          </a>
+          <NavigationMenu
+            className={`w-full ${isChatUI ? "mt-4" : "flex-grow"}`}
+          >
+            <NavigationMenuList
+              className={`flex ${
+                isChatUI
+                  ? "flex-col items-center space-y-4"
+                  : "justify-start space-x-4"
+              }`}
+            >
+              <NavigationMenuItem
+                className={`${isChatUI ? "w-full flex justify-center" : ""}`}
+              >
+                <NavLink
+                  to="/"
+                  className={({ isActive }) => navLinkClass(isActive)}
+                >
+                  <Home className={`mr-0 ${iconColor}`} />
+                  {!isChatUI && <span>Home</span>}
+                </NavLink>
+              </NavigationMenuItem>
+              {!isChatUI && (
                 <Separator
                   className="h-6 w-px bg-zinc-400"
                   orientation="vertical"
                 />
-                <NavigationMenuItem>
+              )}
+              <NavigationMenuItem
+                className={`${isChatUI ? "w-full flex justify-center" : ""}`}
+              >
+                <NavLink
+                  to="/models-deployed"
+                  className={({ isActive }) => navLinkClass(isActive)}
+                >
+                  <BrainCog className={`mr-0 ${iconColor}`} />
+                  {!isChatUI && <span>Models Deployed</span>}
+                </NavLink>
+              </NavigationMenuItem>
+              {isChatUI && (
+                <NavigationMenuItem
+                  className={`${isChatUI ? "w-full flex justify-center" : ""}`}
+                >
                   <NavLink
-                    to="/models-deployed"
+                    to="/chat-ui"
                     className={({ isActive }) => navLinkClass(isActive)}
                   >
-                    <BrainCog className={`mr-1 ${iconColor}`} />
-                    <span className="hidden sm:inline">Models Deployed</span>
+                    <BotMessageSquare className={`mr-0 ${iconColor}`} />
                   </NavLink>
                 </NavigationMenuItem>
-                {chatUIActive && (
-                  <>
-                    <Separator
-                      className="h-6 w-px bg-zinc-400"
-                      orientation="vertical"
-                    />
-                    <NavigationMenuItem>
-                      <NavLink
-                        to="/chat-ui"
-                        className={({ isActive }) => navLinkClass(isActive)}
-                      >
-                        <BotMessageSquare className={`mr-1 ${iconColor}`} />
-                        <span className="hidden sm:inline">Chat UI</span>
-                      </NavLink>
-                    </NavigationMenuItem>
-                  </>
-                )}
-              </NavigationMenuList>
-            </NavigationMenu>
-          </div>
-          <div className="flex items-center space-x-2 sm:space-x-4">
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <div>
-                  <ModeToggle />
-                </div>
-              </TooltipTrigger>
-              <TooltipContent>
-                <div>Toggle Dark/Light Mode</div>
-              </TooltipContent>
-            </Tooltip>
-            <Separator
-              className="h-6 w-px bg-zinc-400"
-              orientation="vertical"
-            />
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <div>
-                  <ResetIcon />
-                </div>
-              </TooltipTrigger>
-              <TooltipContent>
-                <div>Reset Board</div>
-              </TooltipContent>
-            </Tooltip>
-            <Separator
-              className="h-6 w-px bg-zinc-400"
-              orientation="vertical"
-            />
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <div>
-                  <HelpIcon toggleSidebar={handleToggleSidebar} />
-                </div>
-              </TooltipTrigger>
-              <TooltipContent side="left" align="center">
-                <div>Get Help</div>
-              </TooltipContent>
-            </Tooltip>
-          </div>
+              )}
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <div
+          className={`flex ${
+            isChatUI
+              ? "flex-col items-center space-y-4 sm:space-y-6"
+              : "flex-row items-center space-x-4 sm:space-x-6"
+          } ${isChatUI ? "mt-auto" : ""}`}
+        >
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div>
+                <ModeToggle />
+              </div>
+            </TooltipTrigger>
+            <TooltipContent>
+              <div>Toggle Dark/Light Mode</div>
+            </TooltipContent>
+          </Tooltip>
+          <Separator
+            className={`${
+              isChatUI ? "w-10 h-px bg-zinc-400" : "h-6 w-px bg-zinc-400"
+            }`}
+            orientation={isChatUI ? "horizontal" : "vertical"}
+          />
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div>
+                <ResetIcon />
+              </div>
+            </TooltipTrigger>
+            <TooltipContent>
+              <div>Reset Board</div>
+            </TooltipContent>
+          </Tooltip>
+          <Separator
+            className={`${
+              isChatUI ? "w-10 h-px bg-zinc-400" : "h-6 w-px bg-zinc-400"
+            }`}
+            orientation={isChatUI ? "horizontal" : "vertical"}
+          />
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div>
+                <HelpIcon toggleSidebar={handleToggleSidebar} />
+              </div>
+            </TooltipTrigger>
+            <TooltipContent side="left" align="center">
+              <div>Get Help</div>
+            </TooltipContent>
+          </Tooltip>
         </div>
       </div>
       <Sidebar ref={sidebarRef} />
