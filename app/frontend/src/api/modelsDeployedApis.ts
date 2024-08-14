528805f45fc4c554e828920e5cdef15332966b23
anirudTT <aramchandran@tenstorrent.com>
2024-08-14 20:42:22 +0000

    add support to read if model was reset after being deleted

--- app/frontend/src/api/modelsDeployedApis.ts
+++ app/frontend/src/api/modelsDeployedApis.ts
@@ -11,9 +11,11 @@ interface PortBinding {
   HostIp: string;
   HostPort: string;
 }
+
 interface Network {
   DNSNames: string[];
 }
+
 interface ContainerData {
   name: string;
   status: string;
@@ -24,6 +26,7 @@ interface ContainerData {
   port_bindings: { [key: string]: PortBinding[] };
   networks: { [key: string]: Network };
 }
+
 interface Model {
   id: string;
   image: string;
@@ -31,6 +34,18 @@ interface Model {
   health: string;
   ports: string;
   name: string;
+}
+
+interface StopResponse {
+  status: string;
+  stop_response: {
+    status: string;
+    output?: string;
+  };
+  reset_response?: {
+    status: string;
+    output?: string;
+  };
 }
 
 export const fetchModels = async (): Promise<Model[]> => {
@@ -68,27 +83,50 @@ export const fetchModels = async (): Promise<Model[]> => {
   }
 };
 
-export const deleteModel = async (modelId: string): Promise<void> => {
+export const deleteModel = async (modelId: string): Promise<StopResponse> => {
   const truncatedModelId = modelId.substring(0, 4);
   try {
     const payload = JSON.stringify({ container_id: modelId });
     console.log("Payload:", payload);
 
-    const response = await axios.post(stopModelsURL, payload, {
+    const response = await axios.post<StopResponse>(stopModelsURL, payload, {
       headers: {
         "Content-Type": "application/json",
       },
     });
-    console.log("Response:", response);
+    console.log("Response: on ts from backend", response);
 
-    if (response.data.status !== "success") {
+    if (
+      response.data.status !== "success" ||
+      !response.data.stop_response ||
+      response.data.stop_response.status !== "success"
+    ) {
       customToast.error("Failed to stop the container");
       throw new Error("Failed to stop the container");
     } else {
       customToast.success(
         `Model ID: ${truncatedModelId} has been deleted successfully.`
       );
+
+      if (
+        response.data.reset_response &&
+        response.data.reset_response.status === "success"
+      ) {
+        customToast.success(
+          `Model ID: ${truncatedModelId} has been reset successfully.`
+        );
+      } else {
+        customToast.error(`Model ID: ${truncatedModelId} reset failed.`);
+      }
+
+      console.log(
+        `Reset Output: ${
+          response.data.reset_response?.output || "No reset output available"
+        }`
+      );
     }
+
+    return response.data; // Ensure this is returning the correct response
   } catch (error) {
     if (axios.isAxiosError(error)) {
       console.error("Error stopping the container:", error.response?.data);
