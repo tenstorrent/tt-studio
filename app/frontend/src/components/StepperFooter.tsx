42d56398d14b0d9c17c178a51a37e6d84c46b3f8
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-08-21 11:14:36 -0400

    make stepper footter buttons better

--- app/frontend/src/components/StepperFooter.tsx
+++ app/frontend/src/components/StepperFooter.tsx
@@ -1,7 +1,7 @@
 import { useStepper } from "./ui/stepper";
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
-import { RefreshCw, Package } from "lucide-react";
+import { RefreshCw, ExternalLink } from "lucide-react";
 
 const StepperFooter = ({
   removeDynamicSteps,
@@ -23,20 +23,20 @@ const StepperFooter = ({
     <div className="flex items-center justify-end gap-2">
       <Button
         onClick={handleReset}
-        className="flex items-center gap-2 px-4 py-2 bg-gray-700 hover:bg-gray-900 text-white rounded-lg dark:bg-gray-200 dark:hover:bg-gray-400 dark:text-black"
+        className="flex items-center gap-2 px-4 py-2 text-gray-800 bg-gray-300 border border-gray-400 hover:bg-gray-400 hover:text-white rounded-lg dark:bg-gray-700 dark:text-gray-300 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white"
       >
-        <RefreshCw className="w-5 h-5" />
         Reset and Deploy Another Model!
+        <RefreshCw className="w-5 h-5 ml-2" />
       </Button>
 
-      <Button className="flex items-center gap-2 px-4 py-2 bg-gray-700 hover:bg-gray-900 text-white rounded-lg dark:bg-gray-200 dark:hover:bg-gray-400 dark:text-black">
-        <Package className="w-5 h-5" />
+      <Button className="flex items-center gap-2 px-4 py-2 text-gray-800 bg-gray-300 border border-gray-400 hover:bg-gray-400 hover:text-white rounded-lg dark:bg-gray-700 dark:text-gray-300 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white">
         <Link
           to={"/models-deployed"}
-          className="no-underline text-white hover:text-white dark:text-black dark:hover:text-black"
+          className="no-underline text-gray-800 hover:text-white dark:text-gray-300 dark:hover:text-white"
         >
           Models Deployed
         </Link>
+        <ExternalLink className="w-5 h-5 ml-2" />
       </Button>
     </div>
   );
