17fb85a0caf019a8fc180e653c3ebef166a084aa
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-08-23 12:54:53 -0400

    modify es lint to add lc headers

--- app/frontend/.eslintrc.cjs
+++ app/frontend/.eslintrc.cjs
@@ -2,17 +2,25 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "header"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "header/header": [
+      "error",
+      "line",
+      [
+        "# SPDX-License-Identifier: Apache-2.0",
+        "# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC",
+      ],
+    ],
   },
-}
+};
