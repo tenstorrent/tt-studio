72c1c91aaeb442ffecfb6cc1930a18584d8e4d02
anirudTT <aramchandran@tenstorrent.com>
2024-08-26 18:44:29 +0000

    modify to inlcude bash and docker files

--- app/api/add_spdx_header.py
+++ app/api/add_spdx_header.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+# 
+# SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+
 import os
 
 # SPDX header content
@@ -13,9 +17,9 @@ def add_spdx_header(file_path):
             file.seek(0, 0)
             file.write(SPDX_HEADER + "\n" + content)
 
-# Walk through the directory and add the header to all .py files
+# Walk through the directory and add the header to all relevant files
 for root, dirs, files in os.walk("."):
     for file in files:
-        if file.endswith(".py"):  # Check if the file is a Python file
+        if file.endswith((".py", "Dockerfile", ".sh")):  # Check if the file is Python, Dockerfile, or Bash
             file_path = os.path.join(root, file)  # Construct the file path
             add_spdx_header(file_path)  # Pass the file path to the function
