80feaed94d86b361bbc3163d91236bcb61e72b22
Greg Hatt <greg@lightsonsoftware.com>
2024-09-06 11:47:02 -0300

    Adds Axios functions for rag management

---
+++ app/frontend/src/pages/rag/index.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const collectionsAPIURL = "/collections-api";
+
+export const fetchCollections = async () => {
+  const response = await axios.get(`${collectionsAPIURL}/`);
+  console.log(response);
+  if (response?.data) {
+    return response.data;
+  }
+  return [];
+};
+
+export const createCollection = ({
+  collectionName,
+}: {
+  collectionName: string;
+}) => axios.post(`${collectionsAPIURL}/`, { name: collectionName });
+
+export const deleteCollection = async ({
+  collectionName,
+}: {
+  collectionName: string;
+}) => {
+  return axios.delete(`${collectionsAPIURL}/${collectionName}`);
+};
+
+export const uploadDocument = async ({
+  file,
+  collectionName,
+}: {
+  file: File;
+  collectionName: string;
+}) => {
+  return axios.postForm(
+    `${collectionsAPIURL}/${collectionName}/insert_document`,
+    { file }
+  );
+};
