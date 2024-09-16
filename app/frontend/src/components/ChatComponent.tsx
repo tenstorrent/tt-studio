7c5b92618412daea11e5ef785a0b308bcea49af4
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-09-16 14:36:22 -0400

    refactor

--- app/frontend/src/components/ChatComponent.tsx
+++ app/frontend/src/components/ChatComponent.tsx
@@ -6,14 +6,7 @@ import { Button } from "./ui/button";
 import * as ScrollArea from "@radix-ui/react-scroll-area";
 import { useLocation } from "react-router-dom";
 import { Spinner } from "./ui/spinner";
-import {
-  MessageCircle,
-  Smile,
-  CloudSun,
-  Lightbulb,
-  User,
-  ChevronDown,
-} from "lucide-react";
+import { User, ChevronDown } from "lucide-react";
 import { Textarea } from "./ui/textarea";
 import logo from "../assets/tt_logo.svg";
 import {
@@ -32,7 +25,7 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { fetchModels } from "../api/modelsDeployedApis";
-
+import ChatExamples from "./ChatExamples";
 interface InferenceRequest {
   deploy_id: string;
   text: string;
@@ -243,46 +236,7 @@ const ChatComponent: React.FC = () => {
               <p className="text-gray-500 pt-9 font-rmMono">
                 Start a conversation with LLM Studio Chat...
               </p>
-              <div className="mt-4">
-                <div className="flex space-x-4 mt-2">
-                  <Card
-                    className="border border-gray-300 p-4 flex flex-col items-center cursor-pointer rounded-lg hover:bg-zinc-200 dark:hover:bg-zinc-800 transition duration-300"
-                    onClick={() => setTextInput("Hello, how are you today?")}
-                  >
-                    <MessageCircle className="h-6 w-6 mb-2" color="#3b82f6" />
-                    <span className="dark:text-gray-300">
-                      Hello, how are you today?
-                    </span>
-                  </Card>
-                  <Card
-                    className="border border-gray-300 rounded-lg p-4 flex flex-col items-center cursor-pointer hover:bg-zinc-200 dark:hover:bg-zinc-800 transition duration-300"
-                    onClick={() => setTextInput("Can you tell me a joke?")}
-                  >
-                    <Smile className="h-6 w-6 mb-2" color="#be123c" />
-                    <span className="dark:text-gray-300">
-                      Can you tell me a joke?
-                    </span>
-                  </Card>
-                  <Card
-                    className="border border-gray-300 rounded-lg p-4 flex flex-col items-center cursor-pointer hover:bg-zinc-200 dark:hover:bg-zinc-800 transition duration-300"
-                    onClick={() => setTextInput("What's the weather like?")}
-                  >
-                    <CloudSun className="h-6 w-6 mb-2" color="#eab308" />
-                    <span className="dark:text-gray-300">
-                      What's the weather like?
-                    </span>
-                  </Card>
-                  <Card
-                    className="border border-gray-300 rounded-lg p-4 flex flex-col items-center cursor-pointer hover:bg-zinc-200 dark:hover:bg-zinc-800 transition duration-500"
-                    onClick={() => setTextInput("Tell me a fun fact.")}
-                  >
-                    <Lightbulb className="h-6 w-6 mb-2" color="#22c55e" />
-                    <span className="dark:text-gray-300">
-                      Tell me a fun fact.
-                    </span>
-                  </Card>
-                </div>
-              </div>
+              <ChatExamples logo={logo} setTextInput={setTextInput} />
             </div>
           )}
           {chatHistory.length > 0 && (
