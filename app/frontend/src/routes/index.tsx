a4741a9bcbb8eabdbdacf7f9ecb6b6f801e12e72
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-09-21 08:11:04 -0400

    Adds Models Provider

--- app/frontend/src/routes/index.tsx
+++ app/frontend/src/routes/index.tsx
@@ -1,26 +1,29 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
-import React from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ModelsDeployed from "../pages/ModelsDeployed";
 import NavBar from "../components/NavBar";
 import ChatUI from "../pages/ChatUIPage";
 import { RefreshProvider } from "../providers/RefreshContext";
+import { ModelsProvider } from "../providers/ModelsContext";
 import RagManagement from "../components/rag/RagManagement";
 
 const AppRouter = () => {
   return (
     <RefreshProvider>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/models-deployed" element={<ModelsDeployed />} />
-          <Route path="/chat-ui" element={<ChatUI />} />
-          <Route path="/rag-management" element={<RagManagement />} />
-        </Routes>
-      </Router>
+      <ModelsProvider>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/models-deployed" element={<ModelsDeployed />} />
+            <Route path="/chat-ui" element={<ChatUI />} />
+            <Route path="/rag-management" element={<RagManagement />} />
+          </Routes>
+        </Router>
+      </ModelsProvider>
     </RefreshProvider>
   );
 };
