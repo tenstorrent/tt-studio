6a94ea6f45630a045193653794e71c44f663da11
anirudTT <aramchandran@tenstorrent.com>
2024-10-30 15:26:32 +0000

    adds prompt to alert user to only run on certain TT systems

--- setup/steps/step6.sh
+++ setup/steps/step6.sh
@@ -4,6 +4,15 @@ source ./common.sh
 log "Step 6: Setting up Python virtual environment and running topology command"
 load_env
 
+read -p "Is this system a 'TT Loudbox' or 'TT Quiet box'? (Y/N): " system_type
+if [[ "$system_type" != "yes" ]]; then
+    log "⛔ This script can only be run on a 'loudbox' or 'quiet box' system. Exiting.
+    Read more here: https://github.com/tenstorrent/tt-topology"
+    exit 1
+fi
+
+log "Confirmed system type as 'TT Loudbox' or 'TT Quiet Box'"
+
 if [ ! -d "$TT_TOPOLOGY_VENV_PATH" ]; then
     log "Creating Python virtual environment for tt-topology..."
     run_command "create Python virtual environment" "python3 -m venv \"$TT_TOPOLOGY_VENV_PATH\""
@@ -13,14 +22,8 @@ fi
 . "$TT_TOPOLOGY_VENV_PATH/bin/activate"
 log "Activated virtual environment at $TT_TOPOLOGY_VENV_PATH"
 
-
 cd "$TT_TOPOLOGY_DIR" || { log "⛔ Failed to navigate to $TT_TOPOLOGY_DIR"; exit 1; }
 log "📂 Navigated to tt-topology repository at $TT_TOPOLOGY_DIR"
-
-
-if [ -f "requirements.txt" ]; then
-    run_command "install tt-topology dependencies" "pip install -r requirements.txt"
-fi
 
 
 run_command "run tt-topology -l mesh" "./tt-topology -l mesh"
