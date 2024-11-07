5cee903583a19cb576331175e03bb212044a64bf
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2024-11-07 09:55:26 -0500

    anirud/legal-suggestions (#75)
    - removes auto commit on github actions 
    - adds Shadcnn MIT licence 
    - adds correct license headers to Shadcnn UI components in frontend

--- app/frontend/src/components/ui/alert.tsx
+++ app/frontend/src/components/ui/alert.tsx
@@ -1,3 +1,10 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+// SPDX-License-Identifier: Apache-2.0
+// This file incorporates work covered by the following copyright and permission notice:
+//  SPDX-FileCopyrightText: Copyright (c) 2023 shadcn
+//  SPDX-License-Identifier: MIT
+
+
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "../../lib/utils";
--- app/frontend/src/components/ui/aspect-ratio.tsx
+++ app/frontend/src/components/ui/aspect-ratio.tsx
@@ -1,3 +1,10 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+// SPDX-License-Identifier: Apache-2.0
+// This file incorporates work covered by the following copyright and permission notice:
+//  SPDX-FileCopyrightText: Copyright (c) 2023 shadcn
+//  SPDX-License-Identifier: MIT
+
+
 import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
 
 const AspectRatio = AspectRatioPrimitive.Root
