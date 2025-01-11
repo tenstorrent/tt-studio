9f6911698bf925e9cb9f1049f77df265bb2ae865
Benjamin Goel <bgoel@tenstorrent.com>
2025-01-10 19:56:24 -0500

    Add YOLOv4 to studio (#120)
    * Initial commit - add object detection route
    
    * Add package-lock.json
    
    * Add two-column object detection component
    
    * Add new layout and component structure
    
    * Use Aceternity UI file picker
    
    * adds tabs to control menu
    
    * modifies to move webcam to main component
    
    * adds webcam component
    
    * add the react package for webcam util
    
    * add shadcnn tabs ui component
    
    * modifies file upload to show last uploaded file + color change + always show icon to upload
    
    * Fix containing element scroll and z-stack
    
    * Add overflow scroll to main component
    
    * Allow images to assume full width of ObjectDetectionComponent
    
    * Add YoloV4 model config to backend API
    
    * Create new object-detection endpoint & expand DeviceConfigurations enum to support WH_ARCH_YAML setting
    
    * Add ModelType enumeration in frontend to faciliate conditional navigation & use modelID in endpoint invocation
    
    * WIP add components to support:
    - adds a component to open live webcam
    - hit endpoint
    - draws bounding boxes WIP: has errors
    
    * draw box on image
    
    * remove
    
    * Optimize real-time object detection to prevent frame backlog
    
    * Ensure webcam stops completely when stop button is clicked +
    layout changes
    
    * ts fixes
    
    * Fix aspect ratio of video container to 4:3
    
    * Fix navigation and add <img> to SourcePicker component - TODO - wire up API call and detection handling
    
    * Refactor inference API call and UI
    
    * Fix UI bugs
    
    * Add API authentication to YOLOv4 backend
    
    * Address PR comments
    
    ---------
    
    Co-authored-by: Anirudh Ramchandran <aramchandran@tenstorrent.com>

---
+++ app/frontend/src/components/object_detection/utlis/runInference.ts
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+import axios from "axios";
+import { Detection, DetectionMetadata } from "../types/objectDetection";
+import { InferenceRequest } from "../types/objectDetection";
+
+export const runInference = async (
+  request: InferenceRequest,
+  imageSourceElement: HTMLCanvasElement | HTMLImageElement,
+  setDetections: (data: {
+    boxes: Detection[];
+    metadata: DetectionMetadata;
+  }) => void,
+) => {
+  // construct FormData to send to API
+  const formData = new FormData();
+  formData.append("deploy_id", request.deploy_id);
+  // handle Blob and File image sources
+  if (request.imageSource instanceof Blob) {
+    formData.append("image", request.imageSource, "canvas-image.jpg");
+  } else {
+    formData.append("image", request.imageSource);
+  }
+
+  try {
+    const response = await axios.post(
+      `/models-api/object-detection/`,
+      formData,
+      {
+        headers: { "Content-Type": "multipart/form-data" },
+      },
+    );
+    // handle imageSourceElement types
+    let width, height;
+    if (imageSourceElement instanceof HTMLCanvasElement) {
+      width = imageSourceElement.width;
+      height = imageSourceElement.height;
+    } else {
+      width = imageSourceElement.naturalWidth;
+      height = imageSourceElement.naturalHeight;
+    }
+    const detectionMetadata: DetectionMetadata = {
+      width: width,
+      height: height,
+      inferenceTime: response.data.inference_time || 33.333,
+    };
+    const detections: Detection[] = response.data.map(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (item: any) => {
+        const [xmin, ymin, xmax, ymax, confidence, classId, className] = item;
+        const detection: Detection = {
+          xmin,
+          ymin,
+          xmax,
+          ymax,
+          confidence,
+          class: classId,
+          name: className,
+        };
+        return detection;
+      },
+    );
+    setDetections({ boxes: detections, metadata: detectionMetadata });
+  } catch (error) {
+    console.error("Error sending snapshot:", error);
+    throw error;
+  }
+};
