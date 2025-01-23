c1fb45e0ec75ee739964cc06407fae60b6cd5255
bgoelTT <bgoel@tenstorrent.com>
2025-01-23 15:21:18 -0500

    Add ImageGeneration modeltype to navbar and fix ModelID propagation

--- app/frontend/src/components/imageGen/ImageGenParentComponent.tsx
+++ app/frontend/src/components/imageGen/ImageGenParentComponent.tsx
@@ -1,13 +1,33 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from "react-router-dom";
+import { customToast } from "../CustomToaster";
 import { Card } from '../ui/card';
 import ShowcaseGallery from './ShowcaseGallery';
 import StableDiffusionChat from './StableDiffusionChat';
 
 const ImageGenParentComponent: React.FC = () => {
   const [showChat, setShowChat] = useState(false);
+
+  // model handling state
+  const location = useLocation();
+  const [modelID, setModelID] = useState<string | null>(null);
+  const [modelName, setModelName] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (location.state) {
+      if (!location.state.containerID) {
+        customToast.error(
+          "modelID is unavailable. Try navigating here from the Models Deployed tab",
+        );
+        return;
+      }
+      setModelID(location.state.containerID);
+      setModelName(location.state.modelName);
+    }
+  }, [location.state, modelID, modelName]);
 
   return (
     <div className="w-full h-screen p-4 pb-8 pl-32">
