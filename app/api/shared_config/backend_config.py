f820f6f3245dd65a9ade01e8abbfd1e41fb42190
Tom Stesco <tstesco@tenstorrent.com>
2025-01-31 17:22:39 -0500

    Llama 3.x integration using new setup.sh and LLM code base (#164)
    * remove HF token fron .env in tt-studio
    * startup.sh makes HOST_PERSISTENT_STORAGE_VOLUME if it doesnt exist
    * startup.sh uses safety set -euo pipefail
    * remove HF_TOKEN from app/docker-compose.yml
    * remove VLLM_LLAMA31_ENV_FILE now redundant
    * Adding Llama 3.x integration using new setup.sh and LLM code base
    * support multiple models using same container, adds support for MODEL_ID environment variable in tt-inference-server.
    * update volume initialization for new file permissions strategy
    * add SetupTypes to handle different first run and validation behaviour
    * hf_model_id is used to define model_id and model_name if provided (rename hf_model_path to hf_model_id)
    * /home/user/cache_root changed to /home/container_app_user/cache_root
    * fix get_devices_mounts, add mapping
    * use MODEL_ID if in container env_vars to map to impl model config
    * set defaults for ModelImpl
    * add configs for llama 3.x models
    * remove HF_TOKEN from tt-studio .env for ease of setup
    * add environment file processing

--- app/api/shared_config/backend_config.py
+++ app/api/shared_config/backend_config.py
@@ -18,7 +18,6 @@ class BackendConfig:
     weights_dir: str
     model_container_cache_root: str
     jwt_secret: str
-    hf_token: str
 
 
 # environment variables are ideally terminated on import to fail-fast and provide obvious
@@ -34,9 +33,8 @@ backend_config = BackendConfig(
     django_deploy_cache_name="deploy_cache",
     docker_bridge_network_name="tt_studio_network",
     weights_dir="model_weights",
-    model_container_cache_root="/home/user/cache_root",
+    model_container_cache_root="/home/container_app_user/cache_root",
     jwt_secret=os.environ["JWT_SECRET"],
-    hf_token=os.environ["HF_TOKEN"],
 )
 
 # make backend volume if not existing
