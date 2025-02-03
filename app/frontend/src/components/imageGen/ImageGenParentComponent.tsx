7adedb0f427e161e88f0b2c95abb13b667ef1405
anirudTT <aramchandran@tenstorrent.com>
2025-02-03 12:28:58 -0500

    fix ui to be simar to chatui

--- app/frontend/src/components/imageGen/ImageGenParentComponent.tsx
+++ app/frontend/src/components/imageGen/ImageGenParentComponent.tsx
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { customToast } from "../CustomToaster";
-import { Card } from '../ui/card';
-import ShowcaseGallery from './ShowcaseGallery';
-import StableDiffusionChat from './StableDiffusionChat';
+import { Card } from "../ui/card";
+import ShowcaseGallery from "./ShowcaseGallery";
+import StableDiffusionChat from "./StableDiffusionChat";
 
 const ImageGenParentComponent: React.FC = () => {
   const [showChat, setShowChat] = useState(false);
@@ -20,7 +20,7 @@ const ImageGenParentComponent: React.FC = () => {
     if (location.state) {
       if (!location.state.containerID) {
         customToast.error(
-          "modelID is unavailable. Try navigating here from the Models Deployed tab",
+          "modelID is unavailable. Try navigating here from the Models Deployed tab"
         );
         return;
       }
@@ -31,10 +31,13 @@ const ImageGenParentComponent: React.FC = () => {
 
   return (
     <div className="w-full h-screen p-4 pb-8 pl-32">
-      <Card className="flex flex-col w-full h-full overflow-hidden shadow-xl bg-zinc-900/80 border-zinc-800 backdrop-blur-sm">
+      <Card className="flex flex-col w-full h-full overflow-hidden shadow-xl bg-white dark:bg-black border-gray-200 dark:border-[#7C68FA]/20 backdrop-blur-sm">
         <div className="flex-1 overflow-hidden flex flex-col relative">
           {showChat ? (
-            <StableDiffusionChat onBack={() => setShowChat(false)} modelID={modelID}/>
+            <StableDiffusionChat
+              onBack={() => setShowChat(false)}
+              modelID={modelID}
+            />
           ) : (
             <div className="flex-1 overflow-auto">
               <ShowcaseGallery onStartGenerating={() => setShowChat(true)} />
