31eb80593b242c811eb65c1dcba5857dfee531cd
bgoelTT <bgoel@tenstorrent.com>
2025-02-24 12:55:05 -0500

    Remove old ImageGenerationView

--- app/frontend/src/components/imageGen/api/imageGeneration.ts
+++ app/frontend/src/components/imageGen/api/imageGeneration.ts
@@ -26,7 +26,7 @@ export const generateImage = async (
   modelID: string,
   options: ImageGenerationOptions = {}
 ): Promise<string> => {
-  const { useLocalModel = true, localModelUrl = "/models-api/image-generation-v2/" } =
+  const { useLocalModel = true, localModelUrl = "/models-api/image-generation/" } =
     options;
 
   if (useLocalModel) {
