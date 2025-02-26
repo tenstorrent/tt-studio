7b683653554f88a923de864c5d0c2c6d37d834af
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2025-02-26 15:07:15 -0500

    add page to route to ai playground home page

---
+++ app/frontend/src/pages/DeployedHomePage.tsx
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+import { DeployedHome } from "../components/aiPlaygroundHome/DeployedHome";
+
+const DeployedHomePage = () => {
+  return (
+    <>
+      <div className=" h-screen flex-1 w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center ">
+        {/* Radial gradient for the container to give a faded look */}
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white"
+          style={{
+            maskImage:
+              "radial-gradient(ellipse at center, transparent 65%, black 100%)",
+          }}
+        ></div>
+        <div className="flex flex-grow justify-center items-center w-full h-screen">
+          <DeployedHome />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DeployedHomePage;
