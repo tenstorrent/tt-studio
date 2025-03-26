e7e3372390357caddfc3815fbfdea1b5b1c50f7b
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2025-03-26 15:52:44 -0400

    fix lc headers

--- app/frontend/src/routes/index.tsx
+++ app/frontend/src/routes/index.tsx
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
