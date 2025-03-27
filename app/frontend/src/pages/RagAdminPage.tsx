1be7d3eb0400396514f25161b4f5eeebbcebf544
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2025-03-27 10:52:08 -0400

    padding fixes for rag admin page

--- app/frontend/src/pages/RagAdminPage.tsx
+++ app/frontend/src/pages/RagAdminPage.tsx
@@ -14,7 +14,7 @@ const RagAdminPage = () => {
           }}
         ></div>
         <div
-          className="flex flex-grow justify-center items-center w-full h-screen pt-60"
+          className="flex flex-grow justify-center items-center w-full h-screen pt-96"
           style={{ zIndex: 1 }}
         >
           <RagAdmin />
