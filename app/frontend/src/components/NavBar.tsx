ecf5104fb077bfa86444d3684e120b456a896fff
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2025-04-08 12:33:27 -0400

    fix conflicts

--- app/frontend/src/components/NavBar.tsx
+++ app/frontend/src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 
-import React, { useMemo, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, forwardRef } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import {
