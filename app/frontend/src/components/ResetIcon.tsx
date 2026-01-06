c986564e08d4a0eaeb0322e71a0eb1c481e53547
anirudTT <aramchandran@tenstorrent.com>
2026-01-06 14:35:03 -0500

    chore: fix lc headers

--- app/frontend/src/components/ResetIcon.tsx
+++ app/frontend/src/components/ResetIcon.tsx
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+// SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC
 
 import React, { useState, useEffect } from "react";
 import axios from "axios";
