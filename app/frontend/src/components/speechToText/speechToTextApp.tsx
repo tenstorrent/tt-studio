0c07c95410a0026cd1b2e001b61bad38dc5cf553
anirudTT <aramchandran@tenstorrent.com>
2026-03-17 21:01:57 -0400

    update copyright year to 2026 in multiple components

--- app/frontend/src/components/speechToText/speechToTextApp.tsx
+++ app/frontend/src/components/speechToText/speechToTextApp.tsx
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+// SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC
 
 import { useState, useEffect } from "react";
 import { AppSidebar } from "@/src/components/speechToText/appSidebar";
