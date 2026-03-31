d411780460f18ddd3606a02cf4ba6302ad6a48a9
Anirudh Ramchandran <aramchandran@tenstorrent.com>
2026-03-31 16:40:56 -0500

    fix lc headers

--- app/frontend/src/pages/FaceRecognitionPage.tsx
+++ app/frontend/src/pages/FaceRecognitionPage.tsx
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+// SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC
+
+
 import React from "react";
 import FaceRecognitionComponent from "../components/face_recognition/FaceRecognitionComponent";
 
